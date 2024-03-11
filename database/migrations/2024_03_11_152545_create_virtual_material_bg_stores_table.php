3f7e1886b443227edb3a26c2d0bdc21343fc9ec3
Ambaza-Marcelo <ambazamarcellin2001@gmail.com>
2024-03-11 14:26:10 +0100

    virtual material bg store

---
+++ database/migrations/2024_03_11_152545_create_virtual_material_bg_stores_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateVirtualMaterialBgStoresTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('virtual_material_bg_stores', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('virtual_material_bg_stores');
+    }
+}
