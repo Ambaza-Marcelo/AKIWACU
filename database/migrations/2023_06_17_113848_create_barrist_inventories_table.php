79eb05994eddaa63b5d194e3e8e5bebc2ef1fccb
Ambaza-Marcelo <ambazamarcellin2001@gmail.com>
2024-03-11 17:05:37 +0100

    inventory attributes

---
+++ database/migrations/2023_06_17_113848_create_barrist_inventories_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBarristInventoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('barrist_inventories', function (Blueprint $table) {
+            $table->id();
+            $table->string('inventory_no')->nullable(true);
+            $table->unique('inventory_no');
+            $table->string('inventory_signature')->nullable(true);
+            $table->string('date');
+            $table->string('title')->nullable(true);
+            $table->text('description');
+            $table->string('status')->nullable(true);
+            $table->text('rejected_motif')->nullable(true);
+            $table->string('created_by')->nullable(true);
+            $table->string('updated_by')->nullable(true);
+            $table->string('validated_by')->nullable(true);
+            $table->string('confirmed_by')->nullable(true);
+            $table->string('approuved_by')->nullable(true);
+            $table->string('rejected_by')->nullable(true);
+            $table->string('reseted_by')->nullable(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('barrist_inventories');
+    }
+}
